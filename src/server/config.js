1c6e67294079e6edb2c019a8a39caf4419bc2678
adrivelasco <adrivelasco96@hotmail.com>
2018-09-18 14:29:07 -0300

    refactor(): improve server config

--- src/server/config.js
+++ src/server/config.js
@@ -1,7 +1,20 @@
+if (process.env.BROWSER) {
+  throw new Error('Do not import this config file from inside the client-side code.');
+}
+
 const config = {
-  env: process.env.NODE_ENV || 'development',
   host: process.env.HOST || '127.0.0.1',
-  port: process.env.PORT || '4010'
+  port: process.env.PORT || '4010',
+  http_protocol: process.env.HTTP_PROTOCOL || 'http',
+  env: process.env.NODE_ENV || 'development',
+
+  trustProxy: process.env.TRUST_PROXY || 'loopback',
+
+  auth: {
+    jwt: {
+      secret: process.env.JWT_SECRET || 'dummy'
+    }
+  }
 };
 
 module.exports = config;
