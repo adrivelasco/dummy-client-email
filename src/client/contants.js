83df9de6fc9d2f69f74c9cee1594ca1750154ba4
adrivelasco <adrivelasco96@hotmail.com>
2018-09-18 09:33:12 -0300

    feat(): add constants to actions descriptins login

--- src/client/contants.js
+++ src/client/contants.js
@@ -1,2 +1,5 @@
 export const EMAILS_GET_ALL = 'EMAILS_GET_ALL';
 export const EMAILS_GET_BY_ID = 'EMAILS_GET_BY_ID';
+
+export const USERS_LOGIN = 'USERS_LOGIN';
+export const USERS_LOGOUT = 'USERS_LOGOUT';
