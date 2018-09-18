881acf6691f06a62466fec29890203ddfb4a1066
adrivelasco <adrivelasco96@hotmail.com>
2018-09-18 18:03:57 -0300

    feat(redux): login reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 
+import usersReducer from './users';
 import emailsReducer from './emails';
 
 /** * Combine all reducers and export the combined reducers */
 export default combineReducers({
-  emails: emailsReducer
+  emails: emailsReducer,
+  users: usersReducer
 });
