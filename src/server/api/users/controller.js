da90bb1e6a30b0f0455a3992b79ca6fe85dcfa71
adrivelasco <adrivelasco96@hotmail.com>
2018-09-18 18:03:29 -0300

    feat(server): save token on session

--- src/server/api/users/controller.js
+++ src/server/api/users/controller.js
@@ -7,6 +7,10 @@ const controller = {
   login: async (req, res, next) => {
     try {
       const response = await model.login(req.body);
+
+      // Save access-token on session
+      req.session.token = response.token;
+
       res.json(response);
     } catch (err) {
       next(err);
@@ -19,6 +23,10 @@ const controller = {
   logout: async (req, res, next) => {
     try {
       const response = await model.logout();
+
+      // Remove access-token from session
+      delete req.session.token;
+
       res.json(response);
     } catch (err) {
       next(err);
