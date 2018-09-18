7079b7655f1bdb528693c8168321f3b1a1b90569
adrivelasco <adrivelasco96@hotmail.com>
2018-09-18 18:02:31 -0300

    feat(): feat component if require authorization

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import Authenticated from './helpers/Authenticated';
 import ScrollToTop from './helpers/ScrollToTop';
 import Layout from './ui/Layout';
 import views from './views';
 
-const App = () => {
-  return (
-    <ScrollToTop>
-      <Route render={({ location }) => (
-        <Layout location={location}>
-          <Switch>
-            {views.map((view, i) => {
-              return (
-                <Route
-                  key={i}
-                  path={view.path}
-                  exact={view.exact}
-                  pageName={view.title}
-                  render={props => <view.component key={i} {...props} />}
-                />
-              );
-            })}
-          </Switch>
-        </Layout>
-      )} />
-    </ScrollToTop>
-  );
-};
+class App extends Component {
+  renderRouteComponent(props, view) {
+    if (view.requireAuthentication) {
+      return (
+        <Authenticated>
+          <view.component {...props} />;
+        </Authenticated>
+      );
+    }
+    return <view.component {...props} />;
+  }
+
+  render() {
+    return (
+      <ScrollToTop>
+        <Route render={({ location }) => (
+          <Layout location={location}>
+            <Switch>
+              {views.map((view, i) => {
+                return (
+                  <Route
+                    key={i}
+                    path={view.path}
+                    exact={view.exact}
+                    pageName={view.title}
+                    render={props => this.renderRouteComponent(props, view)}
+                  />
+                );
+              })}
+            </Switch>
+          </Layout>
+        )} />
+      </ScrollToTop>
+    );
+  }
+}
 
 export default App;
