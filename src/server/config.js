f981d8466ba5ab2f4a8158ad1ef471c94d42bbb4
adrivelasco <adrivelasco96@hotmail.com>
2018-09-18 11:06:17 -0300

    refactor(): move api config for api directory

--- src/server/config.js
+++ src/server/config.js
@@ -1,14 +1,7 @@
 const config = {
   env: process.env.NODE_ENV || 'development',
-  port: process.env.PORT || '4010',
-
-  api: {
-    basepath: '/mocks',
-    endpoints: {
-      emails: 'emails',
-      users: 'users'
-    }
-  }
+  host: process.env.HOST || '127.0.0.1',
+  port: process.env.PORT || '4010'
 };
 
 module.exports = config;
